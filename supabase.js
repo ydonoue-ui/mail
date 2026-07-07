297768d3d5445df5172b0648396cb60aef0822db
ydonoue-ui <ydonoue@gmail.com>
2026-07-07 15:00:38 +0900

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
-import { SUPABASE_URL, SUPABASE_KEY } from "../config/keys.js";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
